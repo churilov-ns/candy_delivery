a71a61f29544d96fae3acf80495a5b1f586e5b87
churilov-ns <35680494+churilov-ns@users.noreply.github.com>
2021-09-04 18:43:29 +0300

    Update settings.py

--- candy_delivery/settings.py
+++ candy_delivery/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-5qep2b&&h#m@ld1^j%v(!fz*w!46pkz33y3&muej%yipj#$q+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
